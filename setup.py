0c1e7ccb9173202454c295d140b9f360edd441e7
Tim Spriggs <imoverclocked@gmail.com>
2013-03-05 15:39:17 -0700

    add dependencies and bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,20 @@ from distutils.core import setup
 
 setup(
 	name='pbsmake',
-	version="0.1.4",
-	scripts=["pbsmake"]
+	description='Implement job submission and dependencies using a makefile-like syntax',
+	long_description="""
+	This script allows users to create job definitions in a makefile-like
+	syntax. This helps keep job dependencies clear or for the simple case
+	it can help to submit many similar jobs that run slightly differently
+	depending solely on their job name. Invoking a script with pbsmake -f
+	allows wildcard matching and generic commands to be created.
+	EG: create a pbsmake script called "compress_dir" and call it via:
+	% compress_dir *
+	everything that matches * will be created as its own job and suitably
+	compressed.
+	""",
+	version="0.1.6",
+	scripts=["pbsmake"],
+	requires=['pbs (>=0.95)']
 	)
 
